13bd29dcb588441fcc0376ed0ea580216f50d79a
jwong65 <jason.wong6507@gmail.com>
2023-04-06 21:32:22 -0400

    Tested and used eat_lunch

--- functions_practice.py
+++ functions_practice.py
@@ -4,3 +4,18 @@ def hello():
 def pack(i, e, u):
     list = [i, e, u]
     return list
+def eat_lunch(list):
+    if(len(list)==0):
+            print("Lunchbox is empty")
+    for i in list:
+        if (i == list[0]):
+            print("First I eat " + str(i))
+        elif(i != list[0]):
+            print("Next I eat "+ str(i))
+
+
+# Testing each function
+# hello()
+# print(pack(2, 3, 5))
+# eat_lunch([3,4,5])
+# eat_lunch([])
